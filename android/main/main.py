b23a283862b8dc059d053389dfa1b59698699801
imadityacoder <iamadityacoder01@gmail.com>
2024-04-01 21:05:18 +0530

    version 2 done

---
+++ android/main/main.py
@@ -0,0 +1,139 @@
+from pytube import YouTube,Playlist
+from flet import (
+    Page,
+    app,
+    Text,
+    TextField,
+    colors,
+    Row,
+    ThemeMode,
+    icons,
+    Icon,
+    AppView,
+    AppBar,
+    ElevatedButton,
+    alignment,
+    Padding,
+    Theme
+)
+
+
+def main(page:Page):
+    page.title="Welcome to Aditya's Youtube-Video-Downloader"
+    page.vertical_alignment = alignment.top_center
+    page.padding = Padding(top=45,right=0,bottom=0,left=0)
+    page.horizontal_alignment = "center" 
+    page.theme_mode= ThemeMode.LIGHT 
+    page.theme = Theme(color_scheme_seed="pink")  
+    page.bgcolor = colors.PINK_100
+    page.scroll = True
+
+    
+    page.appbar = AppBar(
+        leading=Icon(icons.DOWNLOAD_DONE_OUTLINED),
+        leading_width=25,
+        title=Text("Aditya's Downtube App"),
+        center_title=False,
+        bgcolor=colors.SURFACE_VARIANT,
+    )
+
+    boxh = 80
+    boxw = 290
+    fsize = 14
+    page.window_height = 700
+    page.window_width = 100
+    urltext = Text(
+        value="Enter your URL here:",
+        size=14,
+
+        )
+    
+    page.update()    
+
+    urlbox= TextField(
+        text_size= fsize,
+        border_radius= 8,
+        height=boxh,
+        width=boxw,
+        opacity=0.9,
+        bgcolor=colors.PURPLE_50,
+        border_color="black",
+        hint_text="https://www.youtube.com/watch?v=Vds8ddYXYZY"
+    )
+    output_dir = '/storage/emulated/0/Download'
+
+    def downvideo(e):
+        yt = YouTube(str(urlbox.value))
+        page.add(Text(value=yt.title+" downloading..."))
+        page.update()
+        stream = yt.streams.get_highest_resolution()
+        page.add(Text(value=yt.title+" download in few minutes"))
+        stream.download(output_path=output_dir)
+        page.add(Text(value=yt.title+" downloading done!"))
+        page.update()
+        
+
+    def downplaylist(e):
+        p = Playlist(str(urlbox.value))
+
+        p_len=len(p)
+        no = 1
+
+        page.add(Text(f"Total number of videos in this playlist is {p_len}"))
+        for url in p.video_urls[:p_len]:
+            link = str(url)
+            page.add(Text(f'Downloading video {no} : {link.title}...') )
+            no += 1             
+            yt = YouTube(link)
+            stream = yt.streams.get_highest_resolution()
+            stream.download(output_path=output_dir)
+            page.add(Text(f'Downloading video {no} : {link.title} done!') )
+            page.update() 
+
+
+    def download(e):
+        try:    
+            if "playlist" in urlbox.value:
+                downplaylist(e)
+            else:
+                downvideo(e)
+        except:
+            page.add(Text(f"Sorry! I can't found this '{urlbox.value}' url or\n       You entered the wrong url!"))        
+
+    btndown= ElevatedButton(
+        text="download now",
+        icon=icons.DOWNLOADING_ROUNDED,
+        opacity=0.8,
+        width=boxw-100,
+        height=boxh-40,
+        on_click=lambda e:download(e),
+        )
+
+
+    page.add(
+        Row(
+            [
+                urltext,  
+            ],
+            alignment="center",
+        ),
+        Row(
+            [
+                urlbox,
+            ],
+            alignment="center",
+        ),
+        Row(
+            [
+                btndown,
+            ],
+            alignment="center",
+        ),
+
+    )
+    
+    page.update()
+
+if __name__=="__main__":
+
+    app(target=main)
---
+++ android/v2/main.py
@@ -0,0 +1,308 @@
+from pytube import YouTube,Search
+from time import sleep
+import os
+from flet import (
+    Page,
+    app,
+    Text,
+    colors,
+    ResponsiveRow,
+    ThemeMode,
+    icons,
+    Icon,
+    alignment,
+    Padding,
+    Theme,
+    CupertinoTextField,
+    CupertinoAppBar,
+    Container,
+    IconButton,
+    Column,
+    Row,
+    ProgressBar,
+    MainAxisAlignment,
+    ScrollMode,
+    
+)
+
+
+def main(page:Page):
+    page.vertical_alignment = alignment.top_center
+    page.padding = Padding(top=5,right=0,bottom=2,left=0)
+    page.horizontal_alignment = "center" 
+    page.theme_mode= ThemeMode.LIGHT 
+    page.theme = Theme(color_scheme_seed="green")
+    page.bgcolor = colors.GREEN_100
+    page.scroll = True
+    page.window_height = 740
+    page.window_width = 410
+
+    
+    page.appbar = CupertinoAppBar(
+        leading=Icon(icons.ADOBE_OUTLINED),
+        middle=Text("Aditya's Downtube App"),
+        bgcolor=colors.SURFACE_VARIANT,
+    )
+
+    boxh = 45
+    boxw = 310
+    fsize = 21
+
+    page.update()
+
+    search_box= CupertinoTextField(
+        text_size= fsize,
+        placeholder_text="Search Youtube Videos Here...",
+        border_radius= 0,
+        height=boxh,
+        width=boxw,
+        bgcolor=colors.GREEN_50,
+        prefix=Icon(icons.SEARCH),
+        on_submit=lambda e:[progress(e),searchvideo(e),],
+
+    )
+    search_btn = IconButton(
+        icon=icons.SEARCH,
+        width=45,
+        height=45,
+        bgcolor=colors.GREEN_50,
+        on_click=lambda e:[progress(e),searchvideo(e)],
+    )
+
+    search_results = Column(
+        controls=[
+            Container(
+                content=ResponsiveRow(
+                    controls=[
+                        Text("Welcome to Downtube",size=19,text_align='center'),
+                        Text("Here You can download any youtube video or audio to your gallery",size=18,text_align='center',italic=True),
+                        Text("Author : Aditya Kumar.",size=16,text_align='center',color=colors.BLUE_800),
+                        ],
+                        alignment=alignment.center,
+                    ),
+                    width=400,
+                    height=500,
+                    bgcolor=colors.GREEN_200,
+                    margin=4,
+                    padding=4,
+                    border_radius=6,
+                    alignment=alignment.center
+                    ),
+        ],
+        width=400,
+        height=680,
+        alignment=MainAxisAlignment.START,
+        horizontal_alignment="center",
+        scroll=ScrollMode.AUTO,
+
+    )
+    
+    output_dir = '/storage/emulated/0/Download'
+
+    def searchvideo(e):
+
+        search_results.controls.clear()
+        search = search_box.value
+        query = Search(search)
+
+        try:
+            for result in query.results:
+                search_results.controls.append(
+                    Container(
+                        content=ResponsiveRow(
+                        controls=[
+                        Text(result.title,size=15),
+                         Row(
+                            controls=[
+                                Text('Download '),
+                                IconButton(
+                                    icons.VIDEO_FILE,
+                                    on_click=lambda e:downvideo(e),
+                                    data=str(result.video_id),
+                                    ),
+                                IconButton(
+                                    icons.AUDIO_FILE,
+                                    on_click= lambda e:downaudio(e),
+                                    data=str(result.video_id),
+                                    ),
+                                ]
+                            )
+                        ],
+                        alignment=alignment.center,
+                    ),
+                    width=400,
+                    height=200,
+                    bgcolor=colors.GREEN_200,
+                    margin=6,
+                    padding=4,
+                    border_radius=6,
+                    alignment=alignment.center
+                    ),
+                )
+        except:
+            search_results.controls.append(
+                    Container(
+                        content=ResponsiveRow(
+                        controls=[
+                        Text("Search not Found!",size=23,text_align="center"),
+
+                        ],
+                        alignment= "center",
+                        vertical_alignment="center",
+                    ),
+                    width=400,
+                    height=200,
+                    bgcolor=colors.GREEN_200,
+                    margin=6,
+                    padding=4,
+                    border_radius=6,
+                    alignment=alignment.center,
+                
+                    ),
+                )
+            
+
+        page.update()
+
+    def progress(e):
+        pro = ProgressBar(width=400, color="amber", bgcolor="#eeeeee")
+        search_results.controls.clear()
+        search_results.controls.append(pro)
+        page.update()
+        sleep(3)
+        search_results.controls.remove(pro)
+        page.update()
+
+
+    def downvideo(e):
+        try:
+            url = f'https://www.youtube.com/watch?v={e.control.data}'
+            yt = YouTube(url)
+            search_results.controls.clear()
+            search_results.controls.append(
+                Container(
+                        content=ResponsiveRow(
+                        controls=[
+                        Text(yt.title,size=15),
+                        Text(f'- {yt.author}',size=12),
+                        Text("VIDEO[HD | 1080K ] Downloading...")
+                        ],
+                        alignment=alignment.center,
+                    ),
+                    width=400,
+                    height=200,
+                    bgcolor=colors.GREEN_200,
+                    margin=6,
+                    padding=4,
+                    border_radius=6,
+                    alignment=alignment.center
+                    ),
+            )
+            page.update()
+            stream = yt.streams.get_highest_resolution()
+            stream.download(output_path=output_dir)
+            search_results.controls.clear()
+            search_results.controls.append(
+                Container(
+                        content=ResponsiveRow(
+                        controls=[
+                        Text(yt.title,size=15),
+                        Text(f'- {yt.author}',size=12),
+                        Text("VIDEO[HD | 1080K ] Downloadind done !")
+                        ],
+                        alignment=alignment.center,
+                    ),
+                    width=400,
+                    height=200,
+                    bgcolor=colors.GREEN_200,
+                    margin=6,
+                    padding=4,
+                    border_radius=6,
+                    alignment=alignment.center
+                    ),
+            )
+            page.update()
+
+        except:
+            pass
+
+
+    def downaudio(e):
+        try:
+            url = f'https://www.youtube.com/watch?v={e.control.data}'
+            yt = YouTube(url)
+            search_results.controls.clear()
+            search_results.controls.append(
+                Container(
+                        content=ResponsiveRow(
+                        controls=[
+                        Text(yt.title,size=15),
+                        Text(f'- {yt.author}',size=12),
+                        Text("AUDIO[8D | MP3] Downloading...")
+                        ],
+                        alignment=alignment.center,
+                    ),
+                    width=400,
+                    height=200,
+                    bgcolor=colors.GREEN_200,
+                    margin=6,
+                    padding=4,
+                    border_radius=6,
+                    alignment=alignment.center
+                    ),
+            )
+            page.update()
+            video = yt.streams.filter(only_audio=True).first()
+            downloaded_file = video.download(output_path=output_dir)
+            base, ext = os.path.splitext(downloaded_file)
+            new_file = base + '.mp3'
+            os.rename(downloaded_file, new_file)
+            search_results.controls.clear()
+            search_results.controls.append(
+                Container(
+                        content=ResponsiveRow(
+                        controls=[
+                        Text(yt.title,size=15),
+                        Text(f'- {yt.author}',size=12),
+                        Text("Downloading done !")
+                        ],
+                        alignment=alignment.center,
+                    ),
+                    width=400,
+                    height=200,
+                    bgcolor=colors.GREEN_200,
+                    margin=4,
+                    padding=4,
+                    border_radius=6,
+                    alignment=alignment.center
+                    ),
+            )
+            page.update()
+          
+        except:
+            pass    
+
+
+
+    page.add(
+        
+        ResponsiveRow(
+            [
+                search_box,
+                search_btn
+            ],
+            alignment="center",
+        ),
+        Container(
+            content=search_results,
+            bgcolor= colors.GREEN_50,
+            width=400,
+            height=680,
+            border_radius=8,
+            blur= 5,
+            
+        )
+    )
+    
+    page.update()
+app(target=main)
